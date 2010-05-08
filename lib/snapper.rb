d6782a4396cb4aee5460cca3015a3a78ab7847fc
Evandro Saroka <saroka@gmail.com>
2010-05-08 15:06:50 -0300

    praticamente pronto

--- lib/snapper.rb
+++ lib/snapper.rb
@@ -1,14 +1,14 @@
 class Snapper
-  
+
   attr_reader :testcases
-  
+
   def initialize(file_name)
     File.open(file_name) do |f|
       @testcases = []
       while line = f.gets do
         line_a = line.split(' ').map { |i| i.to_i }
         if line_a.size > 1
-          @testcases << line_a 
+          @testcases << line_a
         else
           @testcases_size = line_a.first
         end
@@ -17,13 +17,25 @@ class Snapper
     raise 'InvalidInput' unless @testcases.size == @testcases_size
   end
   
-  def partial_testcase_solve(opts = {})
-    testcase = testcases[opts[:testcase]]
-    i = opts[:snapper]
-    j = opts[:snap_finger]
-    if j == 0
-      :off
-    end 
+  def solve
+    result = ''
+    testcases.each_with_index { |t, index| result += "#{solve_testcase(t, index)}\n"}
+    result.chomp
   end
   
+  def solve_testcase(t, index)
+    "Case ##{index+1}: #{light(t[0],t[1]) ? 'ON' : 'OFF'}"
+  end
+  
+  def light(i,j)
+    state(i,j) && power(i,j)
+  end
+
+  def state(i,j)
+    j > 0 ? power(i,j-1) ^ state(i,j-1) : false
+  end
+
+  def power(i,j)
+    i > 1 ? light(i-1,j) : true
+  end
 end
--- spec/snapper_spec.rb
+++ spec/snapper_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Snapper do
   describe 'reading input from file' do
-    it 'should build testcases from file' do
+    it 'should build testcases from file and change n to n-1 (zero-based)' do
       s = Snapper.new('spec/testcase.in')
       s.testcases.should have(4).items
       s.testcases[0].should == [1,0]
@@ -10,37 +10,27 @@ describe Snapper do
       s.testcases[2].should == [4,0]
       s.testcases[3].should == [4,47]
     end
-    
+
     it 'should raise InvalidInput when number of testcases are incorrect' do
       lambda { Snapper.new('spec/testcase_invalid.in') }.should raise_error('InvalidInput')
     end
   end
-  
+
   describe 'calculating snapper chain' do
     let(:snapper) { Snapper.new('spec/testcase.in') }
-    it 'should return the initial state of the chain as :off' do
-      snapper.testcases.size.times do |t|
-        snapper.testcases[t].first.times do |i|
-          light_state = snapper.partial_testcase_solve({:testcase => t, :snapper => i, :snap_finger => 0})
-          light_state.should == :off
-        end
+
+    [[1,0,false], [1,1,true], [4,0,false], [4,47,true]].each do |n, k, expected|
+      it "should return the final state of the chain for n = #{n} and k = #{k}" do
+        snapper.light(n,k).should == expected
       end
     end
-    
-    it 'should return the final state of the chain for testcase 0' do
-      snapper.testcase_solve(:testcase => 0).should == :off
-    end
-    
-    it 'should return the final state of the chain for testcase 1' do
-      snapper.testcase_solve(:testcase => 0).should == :on
-    end
-    
-    it 'should return the final state of the chain for testcase 2' do
-      snapper.testcase_solve(:testcase => 0).should == :off
-    end
-    
-    it 'should return the final state of the chain for testcase 3' do
-      snapper.testcase_solve(:testcase => 0).should == :on
+  end
+
+  describe 'calculating snapper chain for all testcases' do
+    let(:snapper) { Snapper.new('spec/testcase.in') }
+
+    it 'should return a string containing results'  do
+      snapper.solve.should == "Case #1: OFF\nCase #2: ON\nCase #3: OFF\nCase #4: ON"
     end
   end
 end
